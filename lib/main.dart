67e8a3d40218e6004327d0a2c76c7dc3c48e3f72
Ngumimi Tuse <tusengumimi21@gmail.com>
2025-12-08 12:44:40 +0100

    chore: move login.dart to screens folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
-import 'colours.dart';
+import 'screens/login.dart';
+import 'ui/colours.dart';
 
 void main() {
   runApp(const MyApp());
